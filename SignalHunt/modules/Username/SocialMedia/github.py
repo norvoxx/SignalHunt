fb2896d4f18b664713e432f5bd7b0f02e91b2a17
ubuntu <foenix208@gmail.com>
2026-05-06 22:49:52 +0200

    Refactor : github.py , creacte class

--- SignalHunt/modules/Username/SocialMedia/github.py
+++ SignalHunt/modules/Username/SocialMedia/github.py
@@ -1,12 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Github:
+    def __init__(self, username:str)->None:
+        self.username = username
+        try:
+            self.resp = requests.get(f'https://api.github.com/users/{username}')
+        except:
+            self.resp = None
 
-def username(username:str)->bool:
-    reponce = requests.get(f'https://api.github.com/users/{username}')
-    try :
-        print(reponce.json()["login"])
-        return True
-    except:
-        print(f'{username} is not a valid username')
-        return False
+    def exist(self)->bool:
+        if self.resp:
+            return True
+        else:
+            return False
+
+    def getAvatar(self)->str:
+        return self.resp.json()['avatar_url']
+
+    def getHtmlUrl(self)->str:
+        return  self.resp.json()['html_url']
+
+
+    def dev(self)->None:
+        if self.resp:
+            print(self.resp.json())
