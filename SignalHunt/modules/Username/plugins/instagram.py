9e93f56cb5c53cc1b4cfb888c6d048666fb95eef
Norvoxx <foenix208@gmail.com>
2026-06-09 16:06:55 +0200

    up instagram.py

--- SignalHunt/modules/Username/plugins/instagram.py
+++ SignalHunt/modules/Username/plugins/instagram.py
@@ -6,15 +6,21 @@ class Instagram(BaseSocialMedia):
     name = "instagram"
     def __init__(self,username):
         super().__init__(username)
+
         self.username = username
+        self.url = f"https://www.instagram.com/{username}"
+        self.exist = False
+        self.requestsData = None
 
         headers = {'User-Agent': 'Mozilla/5.0 MyRedditScraper/1.0'}
-        response = requests.get('https://www.instagram.com/' + username, headers=headers)
+        response = requests.get(self.url, headers=headers)
+
         if response.status_code == 200:
             try:
-                soup = BeautifulSoup(response.content, 'html.parser')
-                self.item = soup.select_one("meta[property='og:description']")
-                self.avatar_meta = soup.select_one("meta[property='og:image']")
+                self.requestsData = BeautifulSoup(response.content, 'html.parser')
+                self.item = self.requestsData.select_one("meta[property='og:description']")
+                if self.item:
+                    self.exist = True
             except:
                 print(f'{username} is not a valid username')
                 self.item = None
@@ -22,31 +28,27 @@ class Instagram(BaseSocialMedia):
             print(f'{username} is not a valid username')
             self.item = None
 
-    def exist(self):
-        if self.item:
-            return True
-        else:
-            return False
-
     def getFollowers(self):
-        return self.item.get("content").split(",")[0]
+        if self.exist:
+            return self.item.get("content").split(",")[0]
+        return None
 
     def getFollowing(self):
-        return  self.item.get("content").split(",")[1].strip()
+        if self.exist:
+            return  self.item.get("content").split(",")[1].strip()
+        return None
 
     def getAvatar(self):
-        if self.avatar_meta:
-            return self.avatar_meta.get("content")
-        return "L'avatar n'a pas pu être récupéré"
+        if self.exist:
+            return  self.requestsData.select_one("meta[property='og:image']").get("content")
+        return None
+
     def API(self):
         return {
-            "exist": self.exist(),
+            "exist": self.exist,
             "website": "instagram",
             "tag": "plugins",
             "username": self.username,
             "avatar": self.getAvatar(),
             "htmlUrl": ('https://www.instagram.com/' + self.username)
         }
-    def dev(self):
-        if self.item:
-            print(self.item)
