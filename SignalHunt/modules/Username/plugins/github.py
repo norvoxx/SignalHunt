d68457f903ed6bb8eca997efff9bbc46deab80b0
Norvoxx <foenix208@gmail.com>
2026-06-09 16:06:43 +0200

    up github.py

--- SignalHunt/modules/Username/plugins/github.py
+++ SignalHunt/modules/Username/plugins/github.py
@@ -2,44 +2,54 @@ import requests
 from modules.Username.base import BaseSocialMedia
 
 class Github(BaseSocialMedia):
+    """
+    A class to represent a Github username
+
+    Attributes:
+        name (str): The name of the social media platform ("github").
+        username (str): Github username.
+        url (str): The GitHub API endpoint for the user.
+        exist (bool): True if the user exists on GitHub, False otherwise.
+        requestsData (dict | None): The raw JSON response from the GitHub API.
+
+    Methods
+        getAvatar(str) : Returns the avatar of the user
+        getHtmlUrl(str) : Returns the html url of the user
+        API(str) : Returns the user's infromation
+    """
+
     name = "github"
-    is_plugin = True
     def __init__(self, username: str) -> None:
+
         super().__init__(username)
 
-        self.data = None
-        self.status_code = None
+        self.username  = username
+        self.url = f"https://api.github.com/users/{username}"
+        self.exist = False
+        self.requestsData = None
 
         try:
-            resp = requests.get(
-                f"https://api.github.com/users/{username}",
-                timeout=5
-            )
-            self.status_code = resp.status_code
-
-            if resp.status_code == 200:
-                self.data = resp.json()
+            response = requests.get(self.url, timeout=5)
+            if response.status_code == 200:
+                self.exist = True
+                self.requestsData = response.json()
 
         except requests.RequestException:
-            self.status_code = None
-            self.data = None
+            pass
 
-    def exist(self) -> bool:
-        return self.status_code == 200
+    def getAvatar(self) -> str | None:
+        if self.exist and self.requestsData:
+            return self.requestsData.get("avatar_url", "")
+        return None
 
-    def getAvatar(self) -> str:
-        if self.exist():
-            return self.data.get("avatar_url", "")
-        return ""
-
-    def getHtmlUrl(self) -> str:
-        if self.exist():
-            return self.data.get("html_url", "")
-        return ""
+    def getHtmlUrl(self) -> str | None:
+        if self.exist and self.requestsData:
+            return self.requestsData.get("html_url", "")
+        return None
 
     def API(self):
         return {
-            "exist": self.exist(),
+            "exist": self.exist,
             "website": self.name,
             "tag": "plugins",
             "username": self.username,
